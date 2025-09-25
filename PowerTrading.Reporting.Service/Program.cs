59db7f731da5cbc58bd1e3fa4097cbb3173ba49f
Elia Markov <elia.markov@outlook.com>
2025-09-25 23:29:46 +0100

    Add logging

--- PowerTrading.Reporting.Service/Program.cs
+++ PowerTrading.Reporting.Service/Program.cs
@@ -1,13 +1,44 @@
 using PowerTrading.Reporting.Service;
+using Serilog;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<IntradayPositionReporter>();
+// Create bootstrap logger for early startup logging
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/powertrading-reporting-bootstrap-.log", 
+        rollingInterval: RollingInterval.Day,
+        retainedFileCountLimit: 7)
+    .CreateBootstrapLogger();
 
-// Add Windows service support
-builder.Services.AddWindowsService(options =>
+try
 {
-    options.ServiceName = "Petroineos PowerTrading Reporting Service";
-});
+    Log.Information("Configuring PowerTrading Reporting Service");
+    
+    var builder = Host.CreateApplicationBuilder(args);
+    
+    builder.Services.AddSerilog((services, lc) => lc
+        .ReadFrom.Configuration(builder.Configuration)
+        .ReadFrom.Services(services));
+    
+    builder.Services.AddHostedService<IntradayPositionReporter>();
 
-var host = builder.Build();
-host.Run();
+    builder.Services.AddWindowsService(options =>
+    {
+        options.ServiceName = "Petroineos PowerTrading Reporting Service";
+    });
+
+    var host = builder.Build();
+    
+    Log.Information("PowerTrading Reporting Service configured successfully");
+    
+    Log.Information("Starting PowerTrading Reporting Service");
+    await host.RunAsync();
+    Log.Information("PowerTrading Reporting Service stopped");
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "PowerTrading Reporting Service terminated unexpectedly");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
